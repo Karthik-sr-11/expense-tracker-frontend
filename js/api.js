bc57fd12b9e47335a2b60f3602e74776fcfecc0b
Karthik-sr-11 <karthik.lance18@gmail.com>
2025-11-29 08:47:52 +0530

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-const API = "https://expense-tracker-backend-aq81.onrender.com/api";
+const API = "https://expense-tracker-backend-aq81.onrender.com/";
 
 function getToken() {
     return localStorage.getItem("token");
@@ -20,5 +20,6 @@ async function apiPost(url, body) {
         body: JSON.stringify(body)
     }).then(res => res.json());
 }
+
 
 
